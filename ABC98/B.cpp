3dd29ae2c486d29d6051fae345cdd43c53bcecb4
tnzk-ren <lotus0r@yahoo.co.jp>
2018-05-26 23:52:23 +0900

    comment+

--- ABC98/B.cpp
+++ ABC98/B.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+//アルファベットを数字（0~25）に変換する関数
 int strToNum(char str)
 {
     string sall = "abcdefghijklmnopqrstuvwxyz";
@@ -21,15 +22,23 @@ int main(void)
     cin >> str;
 
     int max = 0;
+
+    //strの各区切りの総当たり
+    //アルファベットを数字に変換することで、疑似的な連想配列(map)を作っている。
     for(int i = 1; i < N-1; i++){
+        //0~i-1までのa~zの個数をそれぞれカウント
         int cnt_alph1[26] = {0};
         for(int j = 0; j < i; j++){
             cnt_alph1[strToNum(str[j])]++;
         }
+
+        //同じく、i~N-1までのa~zの個数をそれぞれカウント
         int cnt_alph2[26] = {0};
         for(int j = i; j < N; j++){
             cnt_alph2[strToNum(str[j])]++;
         }
+
+        //a~zにおいて分割した文字列のどちらにもある文字を探しカウントアップ
         int cnt = 0;
         for(int k = 0; k < 26; k++){
             if(cnt_alph1[k] != 0 && cnt_alph2[k] != 0){
