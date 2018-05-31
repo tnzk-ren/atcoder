ba821f2a5e4f2e7befd55f75d3a96bd609acf5fc
tnzk-ren <lotus0r@yahoo.co.jp>
2018-05-31 09:12:06 +0900

    AC-D

--- ABC97/D.cpp
+++ ABC97/D.cpp
@@ -1,65 +1,71 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-bool search(vector<int> &vx, vector<int> &vy, int a, int b){
-    vector<int> v;
-    v.push_back(a);
-    for(int i = 0; i < v.size(); i++){
-        //cout << v.size() << endl;
-        for(int j = 0; j < vx.size(); j++){
-            if(vx[j] == v[i]){
-                if(find(v.begin(), v.end(), vy[j]) == v.end()){
-                    v.push_back(vy[j]);
-                }
-            }
-            if(vy[j] == v[i]){
-                if(find(v.begin(), v.end(), vx[j]) == v.end()){
-                    v.push_back(vx[j]);
-                }
-            }
-        }
-    }
-    if(find(v.begin(), v.end(), b) != v.end()){
-        return true;
-    }
-    else{
-        return false;
-    }
+class unionFind{
+public:
+	unionFind(int n);
+	int root(int x);
+	bool same(int x, int y);
+	void unite(int x, int y);
 
+	vector<int> par;
+};
+
+unionFind::unionFind(int n){
+	for (int i = 0; i < n; i++){
+		par.push_back(i);
+	}
 }
 
+int unionFind::root(int x)
+{
+	if (par[x] == x){
+		return x;
+	}
+	else{
+		return par[x] = root(par[x]);
+	}
+}
+
+bool unionFind::same(int x, int y)
+{
+	return root(x) == root(y);
+}
+
+void unionFind::unite(int x, int y)
+{
+	x = root(x);
+	y = root(y);
+	if (x == y)
+		return;
+	par[x] = y;
+}
 
 int main(void)
 {
-    int N, M;
-    cin >> N >> M;
-    vector<int> vp(N);
-    vector<int> vx(M);
-    vector<int> vy(M);
+	int N, M;
 
-    for(int i = 0; i < N; i++){
-        cin >> vp[i];
-    }
-    for(int i = 0; i < M; i++){
-        cin >> vx[i] >> vy[i];
-    }
-    int cnt = 0;
-    vector<int> vs;
-    for(int i = 0; i < N; i++){
-        if(i+1 == vp[i]){
-            cnt++;
-            continue;
-        }
-        if(search(vx, vy, vp[i], i+1)){
-            cnt++;
-        }
-    }
+	cin >> N >> M;
 
-    cout << cnt << endl;
+	vector<int> vp(N);
+	for (int i = 0; i < N; i++){
+		cin >> vp[i];
+	}
+	unionFind *uf = new unionFind(N);
 
-    return 0;
 
+	for (int i = 0; i < M; i++){
+		int x, y;
+		cin >> x >> y;
+		uf->unite(x-1, y-1);
+	}
+	int cnt = 0;
+	for (int i = 0; i < N; i++){
+		if (uf->same(i, vp[i]-1)){
+			cnt++;
+		}
+	}
+	cout << cnt << endl;
 }
